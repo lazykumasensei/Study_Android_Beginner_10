4fc0e5e9de459f2ec2d236a0baa6719b9ce01aba
yccho <52302743+lazykumasensei@users.noreply.github.com>
2020-11-10 16:14:54 +0900

    [main] add project
    - Activity, Fragment, Navigation 을 위한 기본 셋팅

---
+++ app/src/main/java/com/myapp/androidbeginner/MainFragment.kt
@@ -0,0 +1,27 @@
+package com.myapp.androidbeginner
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.f_main.*
+
+class MainFragment: Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.f_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        fMain_text.text = "main"
+        fMain_text.setOnClickListener {
+            findNavController().navigate(R.id.action_mainFragment_to_detailFragment)
+        }
+    }
+}
